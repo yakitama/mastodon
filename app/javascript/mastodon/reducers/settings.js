569724ba7a5758d18be9fffeb8a831ddc5c409c4
yakitama <pikaiv@gmail.com>
2018-08-17 01:43:57 +0900

    LTL が左端、あおのざつだんが右端になるように調整した。

--- app/javascript/mastodon/reducers/settings.js
+++ app/javascript/mastodon/reducers/settings.js
@@ -74,9 +74,10 @@ const initialState = ImmutableMap({
 
 const defaultColumns = fromJS([
   { id: 'COMPOSE', uuid: uuid(), params: {} },
-  { id: 'HASHTAG', uuid: uuid(), params: { id: "あおのざつだん" } },
+  { id: 'COMMUNITY', uuid: uuid(), params: {} },
   { id: 'NOTIFICATIONS', uuid: uuid(), params: {} },
   { id: 'HOME', uuid: uuid(), params: {} },
+  { id: 'HASHTAG', uuid: uuid(), params: { id: "あおのざつだん" } },
 ]);
 
 const hydrate = (state, settings) => state.mergeDeep(settings).update('columns', (val = defaultColumns) => val);
